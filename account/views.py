1cbaae4230ef06e979076ff9da01ab684d14b611
dipeshkhatiwada <deepaacee@gmail.com>
2020-02-09 20:41:51 +0545

    template setup

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+
 
 # Create your views here.
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def signin(request):
+    return render(request, 'login.html')
+
+
+def password_forgot(request):
+    return render(request, 'password_forgot.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
--- accountmanager/urls.py
+++ accountmanager/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from account import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.signin, name='signin'),
+    path('logout/', views.signout, name='signout'),
+    path('password-forgot/', views.password_forgot, name='password_forgot'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
